0aae8aa34887277c53071bca30e6976725626ff0
Reza Alizadeh Majd <r.a.majd@gmail.com>
2018-11-10 15:36:27 +0330

    #4 method `setStatus` from `RPCHandler` implemented.

--- src/RPCHandler.cpp
+++ src/RPCHandler.cpp
@@ -83,6 +83,7 @@ kj::Promise<void> RPCHandler::setStatus(AccountReader::Server::SetStatusContext 
     auto stat = (PXParser::AccountStatus)ctx.getParams().getStat();
 
     KJ_ASSERT(!AccountManager::Instance().setStatus(title, stat), "set account status failed");
+    ctx.getResults().setResult(true);
 
     return kj::READY_NOW;
 }
