7e33e9a4861a883bd0e139ccac5235912f90eafe
Reza Alizadeh Majd <r.a.majd@gmail.com>
2018-11-10 14:51:49 +0330

    method `list` from `RPCHandler` implemented.

--- src/RPCHandler.cpp
+++ src/RPCHandler.cpp
@@ -7,10 +7,34 @@
 #include <string>
 #include <kj/debug.h>
 
+#include "AccountManager.h"
+
 using namespace std;
 
 kj::Promise<void> RPCHandler::list(AccountReader::Server::ListContext ctx) {
-    return Server::list(ctx);
+
+    vector<string> pFilter;
+    vector<string> sFilter;
+
+    if (ctx.getParams().hasProviderFilter()) {
+        for (auto p : ctx.getParams().getProviderFilter()) {
+            pFilter.emplace_back(p.cStr());
+        }
+    }
+    if (ctx.getParams().hasServiceFilter()) {
+        for (auto s : ctx.getParams().getServiceFilter()) {
+            sFilter.emplace_back(s.cStr());
+        }
+    }
+
+    vector<string> accountList = AccountManager::Instance().listAccounts(pFilter, sFilter);
+
+    auto result = ctx.getResults().initAccounts(accountList.size());
+    for (int i = 0; i < accountList.size(); ++i) {
+        result.set(i, accountList[i]);
+    }
+
+    return kj::READY_NOW;
 }
 
 kj::Promise<void> RPCHandler::get(AccountReader::Server::GetContext ctx) {
