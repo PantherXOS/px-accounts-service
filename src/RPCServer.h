5c3830fab18f18b5fb7e8c3976b21f249b7c4cad
Reza Alizadeh Majd <r.majd@pantherx.org>
2020-09-07 13:29:00 +0430

    increase thread timeouts of RPCServer to reduce CPU usage
    Signed-off-by: Reza Alizadeh Majd <r.majd@pantherx.org>

--- src/RPCServer.h
+++ src/RPCServer.h
@@ -58,7 +58,7 @@ public:
 
                 while (instance->isRunning) {
                     waitScope.poll();
-                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
                 }
             }, this);
         }
--- src/main.cpp
+++ src/main.cpp
@@ -72,7 +72,7 @@ int main(int argc, char *argv[]) {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
     while (true) {
-        sleep(1);
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 #pragma clang diagnostic pop
 
